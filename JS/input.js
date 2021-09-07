f01a6621e23707685f61bed724d713bfb2c9d5aa
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-07 15:35:09 +0100

    Add files via upload

--- JS/input.js
+++ JS/input.js
@@ -7,6 +7,18 @@ document.addEventListener('keydown',event => {
 
 alert(event.keycode);
 
+switch(event.keycode){
+case 37:
+    alert("move left");
+    break;
+
+
+
+    case 39:
+        alert('move right');
+}
+
+
 });
 
 
