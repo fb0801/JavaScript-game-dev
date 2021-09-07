7e95c70dea1144891996dee0e1ddc8b9a7c35f77
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-07 19:48:50 +0100

    Add files via upload

--- JS/ball.js
+++ JS/ball.js
@@ -11,6 +11,9 @@ this.gameHeight = game.gameHeight;
 this.position = {x:10, y:10};
 this.speed = {x:4, y:2};
 this.size = 16;//size of the ball
+
+this.game = game;
+
 }
 
 draw(ctx){
@@ -27,13 +30,31 @@ update(){
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
  
+    //wall left of right
     if(this.position.x + this.size > this.gameWidth || this.position.x < 0){
         this.speed.x = -this.speed.x;
     }
 
+    //wall on top or bottom
     if(this.position.y + this.size > this.gameHeight || this.position.y < 0){
         this.speed.y = -this.speed.y;
     }
+
+    //check the ball has hit paddle
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPaddle = this.game.position.y;
+    let leftSideOfPaddle = this.game.paddle.x;
+    let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+
+if (bottomOfBall >= topOfPaddle
+    && this.position.x >=leftSideOfPaddle
+    && this.position.x + this.size <= rightSideOfPaddle
+    
+    ){
+    this.speed.y = - this.speed.y;
+    this.position.y = this.game.paddle.position.y - this.size;
+}
 
 }
 
