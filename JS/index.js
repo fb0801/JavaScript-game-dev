21107b884eb0de6229e2f6ca39b098ca8c2f1a28
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-07 12:25:04 +0100

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,17 @@ let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext("2d");
 
-ctx.fillStyle="#f00";
 
-ctx.fillRect(20, 20, 100, 100);
+
+ctx.clearRect(0,800,600);//clear what was previously in that spot
+
+
+
+ctx.fillStyle="#f00"; //color of shape
+
+ctx.fillRect(20, 20, 100, 100);//size of shape
+
+
+ctx.fillStyle="#00f"; //color of shape
+
+ctx.fillRect(340, 200, 50, 50);//size of shape
