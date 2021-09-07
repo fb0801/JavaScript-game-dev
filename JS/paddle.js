8c381af546f0271bad237124d91c24f1f03af4fd
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-07 12:41:15 +0100

    Add files via upload

--- JS/paddle.js
+++ JS/paddle.js
@@ -3,6 +3,16 @@ class Paddle{
 constructor(game_Width, game_Height){
 this.width = 150;
 this.height=30;
+
+
+
+    this.position ={
+        x:1,
+        y:game_Height - this.height - 10,
+    }
+
+
+
 }
 
 draw(ctx){
