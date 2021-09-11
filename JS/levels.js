49de3e3259cd18a7fa8c10bdd7ae63f6e6751cb0
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-11 11:42:36 +0100

    Add files via upload

---
+++ JS/levels.js
@@ -0,0 +1,22 @@
+import Brick from 'JS/brick';
+
+
+export function buildLevel(level){
+    let bricks = []
+
+
+    level.forEach((row, rowindex) => {
+        row.forEach((brick, brickIndex))
+        
+    });
+}
+
+
+export const level1 =[
+    // 1 to represent where the bricks will be
+    [0,1,0,1,0,1,0,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1]
+
+];
